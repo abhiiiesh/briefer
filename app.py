97bae5a8d7e86db9eea07bbdeec2762d2eca4dd6
abhiiiesh <130227928+abhiiiesh@users.noreply.github.com>
2023-06-18 11:31:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ def generate_description(prompt):
         response = openai.Completion.create(
             engine=model_engine,
             prompt=prompt,
-            max_tokens=700,
+            max_tokens=500,
             n=1,
             stop=None,
             temperature=0.8,
@@ -34,13 +34,22 @@ def home():
         prompt = f"Generate grammatically correct 300 character innovative descriptions for the business. Business name: {company_name}, Established in: {establishment}, Services: {services}, Location: {location}."
 
         descriptions = []
-        description = generate_description(prompt)
-        descriptions.append(description)
-
+        for _ in range(5):
+            description = generate_description(prompt)
+            sentences = description.split('.')
+            generated_description = ""
+            for sentence in sentences:
+                if len(generated_description) + len(sentence) + 1 <= 300:
+                    generated_description += sentence.strip() + '.'
+                else:
+                    break
+            descriptions.append(generated_description)
 
         return render_template('index.html', descriptions=descriptions)
     else:
         return render_template('index.html')
+
+
 
 @app.errorhandler(Exception)
 def handle_error(e):
@@ -51,7 +60,6 @@ def handle_error(e):
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
-
 
 
 
